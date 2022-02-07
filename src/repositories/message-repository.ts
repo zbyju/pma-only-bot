4ba75c806c6eae52634790217e7705ad09c9427c
zbyju <zbynek.jurica@gmail.com>
2022-02-07 16:25:25 +0100

    Add functions to save message

--- src/repositories/message-repository.ts
+++ src/repositories/message-repository.ts
@@ -1,5 +1,30 @@
-import { POBMessage } from "../types/message"
+import { POBMessage } from "../types/message.types"
+import { CallbackError } from "mongoose"
+import { MessageModel } from "../model/messages"
+import Discord from "discord.js"
 
-export const saveMessage = (message: POBMessage) => {
-    
+export const saveMessage = (m: Discord.Message) => {
+    return saveMessagePOB({
+        author: m.author.id,
+        content: m.content,
+        channel: m.channelId,
+        guild: m.guildId,
+        postedAt: m.createdTimestamp,
+        msgId: m.id
+    })
+}
+
+export const saveMessagePOB = (message: POBMessage) => {
+    return new Promise((resolve, reject) => {
+        try {
+            MessageModel.create(message, (err: CallbackError, m: POBMessage) => {
+                if(err) {
+                    reject(err)
+                }
+                resolve(m)
+            })
+        } catch(err) {
+            reject(err)
+        }
+    })
 }
