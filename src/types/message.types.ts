4d5b8ee4d08456f1133f26af778e0c4f0742c0e7
zbyju <zbynek.jurica@gmail.com>
2022-02-07 16:24:40 +0100

    Add type for message from DB

---
+++ src/types/message.types.ts
@@ -0,0 +1,21 @@
+import { Types } from "mongoose";
+
+export interface POBMessage {
+    author: string
+    channel: string
+    guild: string
+    content: string
+    postedAt: number
+    msgId: string
+    tts?: boolean
+    url?: string
+    _id?: Types.ObjectId
+    createdAt?: Date
+    updatedAt?: Date
+}
+
+export interface Saving {
+    lastID: string,
+    channelID: string,
+    guildID: string
+}
