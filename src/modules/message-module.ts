788d93befed13fcb20a1cf82645d9259df7078b5
zbyju <zbynek.jurica@gmail.com>
2022-02-08 15:02:52 +0100

    Add base class for message modules

---
+++ src/modules/message-module.ts
@@ -0,0 +1,12 @@
+import Discord from "discord.js"
+import BaseModule from "./base-module"
+
+export default abstract class MessageModule extends BaseModule {
+    client: Discord.Client<boolean>
+
+    constructor(client: Discord.Client<boolean>) {
+        super(client)
+    }
+
+    abstract onMessage(message: Discord.Message<boolean>): void
+}
