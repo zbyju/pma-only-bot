48c6bf6963232004d6e3c0010f696be10f2f82d7
zbyju <zbynek.jurica@gmail.com>
2022-02-08 15:02:34 +0100

    Add base class for command modules

---
+++ src/modules/command-module.ts
@@ -0,0 +1,15 @@
+import Discord from "discord.js"
+import BaseModule from "./base-module"
+
+export default abstract class CommandModule extends BaseModule {
+    client: Discord.Client<boolean>
+
+    abstract name: string
+    abstract description: string
+
+    constructor(client: Discord.Client<boolean>) {
+        super(client)
+    }
+
+    abstract onCommand(command: Discord.Interaction<Discord.CacheType>): void
+}
