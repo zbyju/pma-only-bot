275b29fd45dd41ff2e602c3afa221a863b765568
zbyju <zbynek.jurica@gmail.com>
2022-02-08 15:02:44 +0100

    Add base class for cron modules

---
+++ src/modules/cron-module.ts
@@ -0,0 +1,16 @@
+import Discord from "discord.js"
+import BaseModule from "./base-module"
+
+export default abstract class CronModule extends BaseModule {
+    client: Discord.Client<boolean>
+
+    // Schedule for node-cron (https://www.npmjs.com/package/node-cron)
+    // Format * * * * * * - sec min hour dom month dow
+    abstract schedule: string
+
+    constructor(client: Discord.Client<boolean>) {
+        super(client)
+    }
+
+    abstract onCron(): void
+}
