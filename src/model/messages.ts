6e013a8232804a09f4d749af08a167d2b345ba0e
zbyju <zbynek.jurica@gmail.com>
2022-02-09 14:44:23 +0100

    Add index for faster message query

--- src/model/messages.ts
+++ src/model/messages.ts
@@ -3,16 +3,18 @@ import { POBMessage } from "../types/message.types"
 
 const schema = new Schema<POBMessage>(
     {
-        author: { type: String, required: true },
+        author: { type: String, required: true, index: true },
         channel: { type: String, required: true },
         guild: { type: String, required: true },
         content: { type: String, required: true },
         postedAt: { type: Number, required: true },
-        msgId: { type: String, required: true, unique: true },
+        msgId: { type: String, required: true, unique: true, index: true },
         tts: { type: Boolean },
         url: { type: String },
     },
     { timestamps: true }
 )
+
+schema.index({ guild: 1, channel: 1, msgId: 1, postedAt: -1 })
 
 export const MessageModel = model<POBMessage>("Message", schema)
